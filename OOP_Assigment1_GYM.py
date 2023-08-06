82efca31ef10229b7a1f1c6e2f4b60fa9fe87331
BelindaWillis001 <116993969+BelindaWillis001@users.noreply.github.com>
2023-08-06 15:35:57 +1200

    sorted getter but not setter

--- OOP_Assigment1_GYM.py
+++ OOP_Assigment1_GYM.py
@@ -29,28 +29,38 @@ class GroupExercise:
     def getClassCheckedin(self):
         return self.classCheckedin
     
+    
 #Define the class Member (cookie cutter) 
 class Member:
     # Define the Member class constructor
     def __init__(self, firstname, lastname, memberID):
-        # Define the Member attributes
-        self.memberFirstname = firstname
-        self.memberLastname = lastname
-        self.memberID = memberID
+        # Define the Member attributes (make them private attributes)
+        self.__memberFirstname = firstname  
+        self.__memberLastname = lastname
+        self.__memberID = memberID
         # create an empty list to store the classes that the member is enrolled in
         self.memberClassList = []
+# Define the private method to display the member's details
+    def __display(self):
+        print("This is a private method")
+    # Define a getter method to return the member's details from the private attributes
+    @property
+    def memberDetails(self):
+        print("This is getting the member details from the private setting....")
+        return self.__memberFirstname + " " + self.__memberLastname + " " + self.__memberID
+
     # Define the Dunder method to return a printable string of Member attributes
     def __str__(self):
-        return ("Member Name: {} {} Member ID: {}".format(self.memberFirstname, self.memberLastname, self.memberID))
+        return ("Member Name: {} {} Member ID: {}".format(self.__memberFirstname, self.__memberLastname, self.__memberID))
     # Define a method to return the classes the member is enrolled in
     def getMemberClasses(self):
         return self.memberClassList
     # Define a method to allow a member to cancel their enrolment in a class
-    def removeClass(self, aClass):
-        self.memberClassList.remove(aClass)
+    def removeClass(self, classToRemove):
+        self.memberClassList.remove(classToRemove)
     # Define a method to allow a member to enrol in a class
-    def addClass(self, aClass):
-        self.memberClassList.append(aClass)
+    def addClass(self, classToAdd):
+        self.memberClassList.append(classToAdd)
 
 
 # Define the class Trainer (cookie cutter)
@@ -68,15 +78,15 @@ class Trainer:
     def getTrainerClasses(self):
         return self.trainerClasses
     # Define a method to allow a trainer to add a class to their list of classes they teach
-    def addtrainerClass(self, aClass):
-        self.trainerClasses.append(aClass)
+    def addTrainerClass(self, classToAdd):
+        self.trainerClasses.append(classToAdd)
 
 # Create the Trainer (objects) - cookies
 trainer1 = Trainer("Sayaka", "Kanayma", "Yoga")
 trainer2 = Trainer("Emma", "McConnell", "Pilates")
 
-print(trainer1)
-print(trainer2)
+# print(trainer1)
+# print(trainer2)
 
 
     
@@ -84,8 +94,8 @@ print(trainer2)
 exerciseclass1 = GroupExercise("Yoga", "Sayaka", 25, 15.00)
 exerciseclass2 = GroupExercise("Pilates", "Emma", 4, 30.00)
 
-print(exerciseclass1)
-print(exerciseclass2)
+# print(exerciseclass1.classDetails())
+# print(exerciseclass2.classDetails())
 
 # Create the member (objects) - cookies
 member1 = Member("Sarah", "Lind", "1")
@@ -94,25 +104,27 @@ member3 = Member("Belinda", "Allison", "3")
 member4 = Member("Peter", "Dunn", "4")
 member5 = Member("Cassie", "Lieu", "5")
     
-print(member1)
-print(member2)
-print(member3)
-print(member4)
-print(member5)
+print(member2.memberDetails)
 
 
 # call the add class method on the member object to add a class to the member's list of classes
 member1.addClass(exerciseclass1)
 member1.addClass(exerciseclass2)
+member2.addClass(exerciseclass1)
+member3.addClass(exerciseclass2)
+
 # print out the class details for all the classes the member is enrolled in
-for aClass in member1.getMemberClasses():
-    print(aClass.classDetails())
+# for aClass in member1.getMemberClasses():
+#     print(aClass.classDetails())
+
+# for aClass in member2.getMemberClasses():
+#     print(aClass.classDetails())
 
 
 # call the add trainer class method on the trainer object to add a class to the trainer's list of classes
-trainer1.addtrainerClass(exerciseclass1)
-trainer2.addtrainerClass(exerciseclass2)
-trainer1.addtrainerClass(exerciseclass2)
+trainer1.addTrainerClass(exerciseclass1)
+trainer2.addTrainerClass(exerciseclass2)
+trainer1.addTrainerClass(exerciseclass2)
 # print out the class details for all the classes the trainer teaches
-for Classes in trainer1.getTrainerClasses():
-    print()
+# for Classes in trainer1.getTrainerClasses():
+#     print(Classes.classDetails())
